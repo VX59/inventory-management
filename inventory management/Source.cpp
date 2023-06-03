a610eaa652522f358c5504af01fb27af1650aa24
Jacob DeRosa <jderosa3@buffalo.edu>
2023-06-02 22:25:56 -0400

    starting multithreaded implementation

--- inventory management/Source.cpp
+++ inventory management/Source.cpp
@@ -5,6 +5,9 @@
 #include <sqlite3.h>
 #include <string>
 #include <fmt/core.h>
+#include <thread>
+#include <chrono>
+#include <mutex>
 
 typedef struct Item {
 	const char* NAME;
@@ -20,24 +23,34 @@ private:
 	const char* wd = "C:\Users\deros\Documents\inventory";
 	static int callback(void* NotUsed, int argc, char** argv, char** asColName) {
 		for (int i = 0; i < argc; i++) {
-			std::cout << fmt::format("|{}: {:<{}}", asColName[i], argv[i], 14);
+			std::cout << fmt::format("|{}: {:<{}}", asColName[i], argv[i], 12);
 		}
 		std::cout << std::endl;
 		return 0;
 	}
+	static void validatePrepareStmt(int ret, sqlite3 *DBhandle) {
+		if (ret != SQLITE_OK) {
+			sqlite3_close(DBhandle);
+			std::cerr << "failed to prepare statment";
+			exit(- 1);
+		}
+	}
+	static void validatestep(int ret, sqlite3* DBhandle) {
+		if (ret != SQLITE_DONE) {
+			sqlite3_close(DBhandle);
+			std::cerr << "failed to execute query";
+			exit(-1);
+		}
+	}
+
+public:
 	int queryDB(std::string query, static int (*cb)(void* NotUsed, int argc, char** argv, char** asColName), char* messageError) {
 		sqlite3* DBhandle;
 		int exit = sqlite3_open(wd, &DBhandle);
 		exit = sqlite3_exec(DBhandle, query.c_str(), cb, 0, &messageError);
 		return exit;
 	}
-	float isNumber(const char* input) {
-		for (int i = 0; input[i] != '\0'; i++) {
-			if (!(isdigit(input[i])) && input[i] != '.') return -1;
-		}
-		return std::stof(input);
-	}
-public: 
+
 	int createDB() {
 		sqlite3* DBhandle;
 		sqlite3_open(wd, &DBhandle);
@@ -51,7 +64,7 @@ public:
 			"ID integer PRIMARY KEY,"
 			"NAME TEXT,"
 			"UNITS INT,"
-			"PRICE INT,"
+			"PRICE FLOAT,"
 			"CAPACITY INT,"
 			"CATEGORY TEXT)";
 
@@ -77,11 +90,8 @@ public:
 		sqlite3_stmt* sqlStmt = nullptr;
 		std::string query = "INSERT INTO items (ID, NAME, UNITS, PRICE, CAPACITY, CATEGORY) VALUES( ?1, ?2, ?3, ?4, ?5, ?6);";
 		exit = sqlite3_prepare_v2(DBhandle, query.c_str(), query.size() + 1, &sqlStmt, nullptr);
-		if (exit != SQLITE_OK) {
-			sqlite3_close(DBhandle);
-			std::cout << "failed to prepare statment";
-			return -1;
-		}
+		validatePrepareStmt(exit, DBhandle);
+
 		sqlite3_bind_int(sqlStmt, 1, record->ID);
 		sqlite3_bind_text(sqlStmt, 2, record->NAME, -1, NULL);
 		sqlite3_bind_int(sqlStmt, 3, record->UNITS);
@@ -90,19 +100,56 @@ public:
 		sqlite3_bind_text(sqlStmt, 6, record->CATEGORY, -1, NULL);
 
 		exit = sqlite3_step(sqlStmt);
-		if(exit != SQLITE_DONE) {
-			sqlite3_close(DBhandle);
-			std::cout << "failed to execute query";
-			return -1;
-		}
+		validatestep(exit, DBhandle);
 		sqlite3_finalize(sqlStmt);
-
 		return 0;
 	}
 
 	int showAllRecords() {
 		std::string query = "SELECT * FROM items;";
 		int exit = queryDB(query, callback, NULL);
+		return 0;
+	}
+
+	int updatePrice(float newval, const char* condval, bool incr = false) {
+		char* messageError;
+		sqlite3* DBhandle;
+		int exit = sqlite3_open(wd, &DBhandle);
+		sqlite3_stmt* sqlStmt = nullptr;
+		std::string query = "UPDATE items SET PRICE = ?1 WHERE NAME = ?2;";
+		std::string incr_query = "UPDATE items SET PRICE = UNITS + ?1 WHERE NAME = ?2";
+		if (incr == true) exit = sqlite3_prepare_v2(DBhandle, incr_query.c_str(), incr_query.size() + 1, &sqlStmt, nullptr);
+		else exit = sqlite3_prepare_v2(DBhandle, query.c_str(), query.size() + 1, &sqlStmt, nullptr);
+		validatePrepareStmt(exit, DBhandle);
+
+		sqlite3_bind_double(sqlStmt, 1, newval);
+		sqlite3_bind_text(sqlStmt, 2, condval, -1, NULL);
+
+		exit = sqlite3_step(sqlStmt);
+		validatestep(exit, DBhandle);
+		sqlite3_finalize(sqlStmt);
+
+		return 0;
+	}
+
+	int updateCapacity(int newval, const char* condval, bool incr = false) {
+		char* messageError;
+		sqlite3* DBhandle;
+		int exit = sqlite3_open(wd, &DBhandle);
+		sqlite3_stmt* sqlStmt = nullptr;
+		std::string query = "UPDATE items SET CAPACITY = ?1 WHERE NAME = ?2;";
+		std::string incr_query = "UPDATE items SET CAPACITY = UNITS + ?1 WHERE NAME = ?2";
+		if (incr == true) exit = sqlite3_prepare_v2(DBhandle, incr_query.c_str(), incr_query.size() + 1, &sqlStmt, nullptr);
+		else exit = sqlite3_prepare_v2(DBhandle, query.c_str(), query.size() + 1, &sqlStmt, nullptr);
+		validatePrepareStmt(exit, DBhandle);
+
+		sqlite3_bind_int(sqlStmt, 1, newval);
+		sqlite3_bind_text(sqlStmt, 2, condval, -1, NULL);
+
+		exit = sqlite3_step(sqlStmt);
+		validatestep(exit, DBhandle);
+		sqlite3_finalize(sqlStmt);
+
 		return 0;
 	}
 
@@ -115,42 +162,75 @@ public:
 		std::string incr_query = "UPDATE items SET UNITS = UNITS + ?1 WHERE NAME = ?2";
 		if(incr == true) exit = sqlite3_prepare_v2(DBhandle, incr_query.c_str(), incr_query.size() + 1, &sqlStmt, nullptr);
 		else exit = sqlite3_prepare_v2(DBhandle, query.c_str(), query.size() + 1, &sqlStmt, nullptr);
+		validatePrepareStmt(exit, DBhandle);
 
-		if (exit != SQLITE_OK) {
-			sqlite3_close(DBhandle);
-			std::cout << "failed to prepare statment";
-			return -1;
-		}
 		sqlite3_bind_int(sqlStmt, 1, newval);
 		sqlite3_bind_text(sqlStmt, 2, condval, -1, NULL);
 
 		exit = sqlite3_step(sqlStmt);
-		if (exit != SQLITE_DONE) {
-			sqlite3_close(DBhandle);
-			std::cout << "Error updating record";
-			return -1;
-		}
+		validatestep(exit, DBhandle);
 		sqlite3_finalize(sqlStmt);
 
 		return 0;
 	}
-
+	int removeRecord(const char* name) {
+		char* messageError;
+		sqlite3* DBhandle;
+		sqlite3_open(wd, &DBhandle);
+		sqlite3_stmt* sqlStmt = nullptr;
+		std::string query = "DELETE FROM items WHERE NAME = ?1";
+		int exit = sqlite3_prepare_v2(DBhandle, query.c_str(), query.size() + 1, &sqlStmt, nullptr);
+		validatePrepareStmt(exit, DBhandle);
+		sqlite3_bind_text(sqlStmt, 1, name, -1, NULL);
+		exit = sqlite3_step(sqlStmt);
+		validatestep(exit, DBhandle);
+		sqlite3_finalize(sqlStmt);
+		return 0;
+	}
 	int dropTable() {
 		char* messageError = (char*)malloc(1024);
 		std::string query("DROP TABLE items;");
-		int exit = queryDB(query, callback, messageError);
+		int exit = queryDB(query, NULL, messageError);
 		return 0;
 	}
-};
+} Inventory;
 
+std::mutex mtx;
+
+void consumer(void) {
+	for (int i = 0; i < 10; i++) {
+		//std::this_thread::sleep_for(std::chrono::milliseconds(20));
+		mtx.lock();
+		Inventory.updateUnit(-1, "gold", true);
+		Inventory.showAllRecords();
+		mtx.unlock();
+		std::cout << "Thread b Reporting: " << i << std::endl;
+
+	}
+}
+void producer(void) {
+	for (int i = 0; i < 10; i++) {
+		//std::this_thread::sleep_for(std::chrono::milliseconds(20));
+		mtx.lock();
+		Inventory.updateUnit(2, "gold", true);
+		Inventory.showAllRecords();
+
+		mtx.unlock();
+		std::cout << "Thread a Reporting: " << i << std::endl;
+	}
+}
 int main() {
-	SQLDatabase Inventory;
-	Item item = { "weed", "drug", 7.00, 3, 1000, 31.1 };
+	std::thread pthreada(consumer);
+	std::thread pthreadb(producer);
+	//Item item = { "iron", "metal", 15.00, 1, 500, 10 };
 	//Inventory.createTable();
 	//Inventory.insertRecord(&item);
 	//Inventory.showAllRecords();
-	Inventory.updateUnit(-25, "weed", true);
-	Inventory.showAllRecords();
+	//Inventory.updatePrice(25, "iron");
+	//Inventory.updateCapacity(1500, "iron");
+	//Inventory.removeRecord("iron");
+	pthreada.join();
+	pthreadb.join();
 	//Inventory.dropTable();
 	return 0;
 }
